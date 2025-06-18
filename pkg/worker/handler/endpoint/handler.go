08c2a68f40c3cea2d4f017c110634ff0b406394c
xh3b4sd <xh3b4sd@gmail.com>
2025-06-18 15:44:46 +0200

    add metrics exporter for http endpoint health (#8)

--- pkg/worker/handler/endpoint/handler.go
+++ pkg/worker/handler/endpoint/handler.go
@@ -1,4 +1,4 @@
-package keypair
+package endpoint
 
 import (
 	"fmt"
@@ -6,53 +6,71 @@ import (
 	"github.com/0xSplits/specta/pkg/envvar"
 	"github.com/0xSplits/specta/pkg/recorder"
 	"github.com/0xSplits/specta/pkg/registry"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/xh3b4sd/logger"
 	"github.com/xh3b4sd/tracer"
 	"go.opentelemetry.io/otel/metric"
 )
 
 const (
-	Metric = "aws_ec2_keypair_total"
+	Metric = "http_endpoint_health"
+)
+
+var (
+	endpoint = map[string]map[string]string{
+		"server": {
+			"testing":    "https://test.api.splits.org/metrics",
+			"staging":    "https://beta.api.splits.org/metrics",
+			"production": "https://api.splits.org/metrics",
+		},
+		"specta": {
+			"testing":    "https://specta.testing.splits.org/metrics",
+			"staging":    "https://specta.staging.splits.org/metrics",
+			"production": "https://specta.production.splits.org/metrics",
+		},
+	}
 )
 
 type Config struct {
-	Aws aws.Config
 	Env envvar.Env
 	Log logger.Interface
 	Met metric.Meter
+	Ser string
 }
 
 type Handler struct {
-	ec2 *ec2.Client
+	end string
 	env envvar.Env
 	log logger.Interface
 	reg registry.Interface
+	ser string
 }
 
 func New(c Config) *Handler {
-	if c.Aws.Region == "" {
-		tracer.Panic(tracer.Mask(fmt.Errorf("%T.Aws must not be empty", c)))
-	}
 	if c.Log == nil {
 		tracer.Panic(tracer.Mask(fmt.Errorf("%T.Log must not be empty", c)))
 	}
 	if c.Met == nil {
 		tracer.Panic(tracer.Mask(fmt.Errorf("%T.Met must not be empty", c)))
 	}
+	if c.Ser == "" {
+		tracer.Panic(tracer.Mask(fmt.Errorf("%T.Ser must not be empty", c)))
+	}
 
 	cou := map[string]recorder.Interface{}
 
+	gau := map[string]recorder.Interface{}
+
 	{
-		cou[Metric] = recorder.NewCounter(recorder.CounterConfig{
-			Des: "the total amount of EC2 key-pairs",
+		gau[Metric] = recorder.NewGauge(recorder.GaugeConfig{
+			Des: "the health status of an http endpoint",
+			Lab: map[string][]string{
+				"service": {"server", "specta"},
+			},
 			Met: c.Met,
 			Nam: Metric,
 		})
 	}
 
-	gau := map[string]recorder.Interface{}
 	his := map[string]recorder.Interface{}
 
 	var reg registry.Interface
@@ -68,9 +86,10 @@ func New(c Config) *Handler {
 	}
 
 	return &Handler{
-		ec2: ec2.NewFromConfig(c.Aws),
+		end: endpoint[c.Ser][c.Env.Environment],
 		env: c.Env,
 		log: c.Log,
 		reg: reg,
+		ser: c.Ser,
 	}
 }
