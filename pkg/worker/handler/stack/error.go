7c0f2a852d97ee65788eda45695eb3db7f5abcdf
xh3b4sd <xh3b4sd@gmail.com>
2025-06-24 17:09:33 +0200

    use sdk pagination for accurate stack status instrumentation (#15)

--- pkg/worker/handler/stack/error.go
+++ pkg/worker/handler/stack/error.go
@@ -6,10 +6,5 @@ import (
 
 var missingRootStackError = &tracer.Error{
 	Kind: "missingRootStackError",
-	Desc: "The exporter expected to find exactly one root stack, but no stack name matched against the internal mapping.",
-}
-
-var tooManyRootStacksError = &tracer.Error{
-	Kind: "tooManyRootStacksError",
-	Desc: "The exporter expected to find exactly one root stack, but more than one stack names matched against the internal mapping.",
+	Desc: "The exporter expected to find exactly one root stack, but no stack was found for teh given environment.",
 }
--- pkg/worker/handler/stack/stack.go
+++ pkg/worker/handler/stack/stack.go
@@ -1,47 +1,51 @@
 package stack
 
 import (
-	"context"
+	"regexp"
 	"strings"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
-	"github.com/xh3b4sd/tracer"
 )
 
-var status = map[types.ResourceStatus]float64{
-	types.ResourceStatusCreateFailed:             0.0,
-	types.ResourceStatusDeleteFailed:             0.0,
-	types.ResourceStatusDeleteSkipped:            0.0,
-	types.ResourceStatusExportFailed:             0.0,
-	types.ResourceStatusExportRollbackComplete:   0.0,
-	types.ResourceStatusExportRollbackFailed:     0.0,
-	types.ResourceStatusExportRollbackInProgress: 0.0,
-	types.ResourceStatusImportFailed:             0.0,
-	types.ResourceStatusImportRollbackComplete:   0.0,
-	types.ResourceStatusImportRollbackFailed:     0.0,
-	types.ResourceStatusImportRollbackInProgress: 0.0,
-	types.ResourceStatusRollbackComplete:         0.0,
-	types.ResourceStatusRollbackFailed:           0.0,
-	types.ResourceStatusRollbackInProgress:       0.0,
-	types.ResourceStatusUpdateFailed:             0.0,
-	types.ResourceStatusUpdateRollbackComplete:   0.0,
-	types.ResourceStatusUpdateRollbackFailed:     0.0,
-	types.ResourceStatusUpdateRollbackInProgress: 0.0,
+var (
+	// exp defines a regular expression for the stack names of CloudFormation
+	// stacks. Note that there is no precise definition for the amount of
+	// characters that a random hash suffix for the stack names ought to look
+	// like. So we are testing against at least 10 characters, but also accept
+	// e.g. 13 or 14.
+	//
+	//     server-test-FargateStack-QGXQ9XZ4J44K
+	//
+	exp = regexp.MustCompile(`-[A-Z0-9]{10,}$`)
 
-	types.ResourceStatusCreateInProgress: 0.5,
-	types.ResourceStatusDeleteInProgress: 0.5,
-	types.ResourceStatusExportInProgress: 0.5,
-	types.ResourceStatusImportInProgress: 0.5,
-	types.ResourceStatusUpdateInProgress: 0.5,
+	status = map[types.StackStatus]float64{
+		types.StackStatusCreateFailed:                            0.0,
+		types.StackStatusDeleteFailed:                            0.0,
+		types.StackStatusImportRollbackComplete:                  0.0,
+		types.StackStatusImportRollbackFailed:                    0.0,
+		types.StackStatusImportRollbackInProgress:                0.0,
+		types.StackStatusRollbackComplete:                        0.0,
+		types.StackStatusRollbackFailed:                          0.0,
+		types.StackStatusRollbackInProgress:                      0.0,
+		types.StackStatusUpdateFailed:                            0.0,
+		types.StackStatusUpdateRollbackComplete:                  0.0,
+		types.StackStatusUpdateRollbackCompleteCleanupInProgress: 0.0,
+		types.StackStatusUpdateRollbackFailed:                    0.0,
+		types.StackStatusUpdateRollbackInProgress:                0.0,
 
-	types.ResourceStatusCreateComplete: 1.0,
-	types.ResourceStatusDeleteComplete: 1.0,
-	types.ResourceStatusExportComplete: 1.0,
-	types.ResourceStatusImportComplete: 1.0,
-	types.ResourceStatusUpdateComplete: 1.0,
-}
+		types.StackStatusCreateInProgress:                0.5,
+		types.StackStatusDeleteInProgress:                0.5,
+		types.StackStatusImportInProgress:                0.5,
+		types.StackStatusReviewInProgress:                0.5,
+		types.StackStatusUpdateCompleteCleanupInProgress: 0.5,
+		types.StackStatusUpdateInProgress:                0.5,
+
+		types.StackStatusCreateComplete: 1.0,
+		types.StackStatusDeleteComplete: 1.0,
+		types.StackStatusImportComplete: 1.0,
+		types.StackStatusUpdateComplete: 1.0,
+	}
+)
 
 type stack struct {
 	// hlt is the CloudFormation stack status, either 0.0, 0.5 or 1.0.
@@ -57,57 +61,45 @@ type stack struct {
 // assigned the stack status 0.5, otherwise the unhealthy stack status 0 is
 // assigned.
 func (h *Handler) stack(det []detail) ([]stack, error) {
-	var err error
-
 	var sta []stack
+
 	for _, x := range det {
-		var inp *cloudformation.DescribeStackResourcesInput
+		var tag string
 		{
-			inp = &cloudformation.DescribeStackResourcesInput{
-				StackName: aws.String(x.arn),
+			tag = staTag(x.nam)
+		}
+
+		if tag == "" {
+			h.log.Log(
+				"level", "warning",
+				"message", "skipping instrumentation for CloudFormation stack",
+				"reason", "CloudFormation stack name pattern is unrecognizable",
+				"name", x.nam,
+			)
+
+			{
+				continue
 			}
 		}
 
-		var out *cloudformation.DescribeStackResourcesOutput
+		var hlt float64
 		{
-			out, err = h.cfc.DescribeStackResources(context.Background(), inp)
-			if err != nil {
-				return nil, tracer.Mask(err)
-			}
+			hlt = status[x.sta]
 		}
 
-		for _, y := range out.StackResources {
-			var tag string
-			{
-				tag = staTag(*y.LogicalResourceId)
-			}
-
-			if tag == "" {
-				h.log.Log(
-					"level", "warning",
-					"message", "skipping instrumentation for CloudFormation stack",
-					"reason", "CloudFormation stack name pattern is unrecognizable",
-					"name", *y.LogicalResourceId,
-				)
-
-				{
-					continue
-				}
-			}
-
-			var hlt float64
-			{
-				hlt = status[y.ResourceStatus]
-			}
-
-			sta = append(sta, stack{
-				hlt: hlt,
-				lab: tag,
-			})
-		}
+		sta = append(sta, stack{
+			hlt: hlt,
+			lab: tag,
+		})
 	}
 
 	return sta, nil
+}
+
+// rooSta identifies whether the given CloudFormation stack name is considered a
+// root stack name.
+func rooSta(arn string) bool {
+	return !exp.MatchString(arn)
 }
 
 func staTag(nam string) string {
@@ -115,6 +107,10 @@ func staTag(nam string) string {
 		if strings.Contains(nam, k) {
 			return v
 		}
+	}
+
+	if rooSta(nam) {
+		return "root"
 	}
 
 	return ""
