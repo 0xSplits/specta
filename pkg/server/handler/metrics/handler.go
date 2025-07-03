10c0825f42db8f32bbe805cfe180b0c15fd98f06
xh3b4sd <xh3b4sd@gmail.com>
2025-07-03 13:33:30 +0200

    whitelist page ready histogram for frontend slo

--- pkg/server/handler/metrics/handler.go
+++ pkg/server/handler/metrics/handler.go
@@ -33,45 +33,29 @@ func New(c Config) *Handler {
 
 	cou := map[string]recorder.Interface{}
 
-	{
-		nam := "teams_bridge_total"
-		cou[nam] = recorder.NewCounter(recorder.CounterConfig{
-			Des: "the total amount of bridge transactions",
-			Lab: map[string][]string{
-				"success": {"true", "false"},
-			},
-			Met: c.Met,
-			Nam: nam,
-		})
-	}
-
 	gau := map[string]recorder.Interface{}
-
-	{
-		// gauges can be registered here
-	}
 
 	his := map[string]recorder.Interface{}
 
 	{
-		nam := "teams_bridge_duration_seconds"
+		nam := "page_ready_duration_seconds"
 		his[nam] = recorder.NewHistogram(recorder.HistogramConfig{
-			Des: "the time it takes for bridge transactions to complete",
+			Des: "the time it takes for the app to show a page",
 			Lab: map[string][]string{
-				"success": {"true", "false"},
+				"page": {"root"},
 			},
 			Buc: []float64{
-				0.1, //     100ms
-				0.5, //     500ms
-				1.0, //   1,000ms
-				2.5, //   2,500ms
-				5.0, //   5,000ms
+				0.1, //   100ms
+				0.2, //   200ms
+				0.3, //   300ms
+				0.4, //   400ms
+				0.5, //   500ms
 
-				10.0, // 10,000ms
-				15.0, // 15,000ms
-				20.0, // 20,000ms
-				25.0, // 25,000ms
-				30.0, // 30,000ms
+				1.0, // 1,000ms
+				2.0, // 2,000ms
+				3.0, // 3,000ms
+				4.0, // 4,000ms
+				5.0, // 5,000ms
 			},
 			Met: c.Met,
 			Nam: nam,
