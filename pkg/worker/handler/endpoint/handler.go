d8ed58a22c6124d16c852791792c3199683b86fe
xh3b4sd <xh3b4sd@gmail.com>
2025-07-17 11:48:49 +0200

    set client endpoints to new url scheme

--- pkg/worker/handler/endpoint/handler.go
+++ pkg/worker/handler/endpoint/handler.go
@@ -16,29 +16,26 @@ const (
 )
 
 var (
-	mapping = map[string]map[string]string{
-		"api": {
-			"production": "https://api.splits.org/metrics",
-		},
+	mapping = map[string]map[string][]string{
 		"explorer": {
-			"testing":    "https://test.app.splits.org",
-			"staging":    "https://dev.app.splits.org",
-			"production": "https://app.splits.org",
+			"testing":    {"https://explorer.testing.splits.org"},
+			"staging":    {"https://explorer.staging.splits.org"},
+			"production": {"https://explorer.production.splits.org", "https://app.splits.org"},
 		},
 		"server": {
-			"testing":    "https://server.testing.splits.org/metrics",
-			"staging":    "https://server.staging.splits.org/metrics",
-			"production": "https://server.production.splits.org/metrics", // api.splits.org
+			"testing":    {"https://server.testing.splits.org/metrics"},
+			"staging":    {"https://server.staging.splits.org/metrics"},
+			"production": {"https://server.production.splits.org/metrics", "api.splits.org"},
 		},
 		"specta": {
-			"testing":    "https://specta.testing.splits.org/metrics",
-			"staging":    "https://specta.staging.splits.org/metrics",
-			"production": "https://specta.production.splits.org/metrics",
+			"testing":    {"https://specta.testing.splits.org/metrics"},
+			"staging":    {"https://specta.staging.splits.org/metrics"},
+			"production": {"https://specta.production.splits.org/metrics"},
 		},
 		"teams": {
-			"testing":    "https://test.teams.splits.org",
-			"staging":    "https://dev.teams.splits.org",
-			"production": "https://teams.splits.org",
+			"testing":    {"https://teams.testing.splits.org"},
+			"staging":    {"https://teams.staging.splits.org"},
+			"production": {"https://teams.production.splits.org", "https://teams.splits.org"},
 		},
 	}
 )
@@ -71,7 +68,7 @@ func New(c Config) *Handler {
 		gau[Metric] = recorder.NewGauge(recorder.GaugeConfig{
 			Des: "the health status of an http endpoint",
 			Lab: map[string][]string{
-				"service": {"api", "explorer", "server", "specta", "teams"},
+				"service": {"explorer", "server", "specta", "teams"},
 			},
 			Met: c.Met,
 			Nam: Metric,
